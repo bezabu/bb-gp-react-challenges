362bd000d060c7862793db631e861532be1db239
Ben Butler <benbutleranimator@gmail.com>
2024-04-12 15:31:20 +0000

    Updating state challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar.js';
+import NavBarSimple from './components/NavBarSimple.js'
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      <NavBarSimple />
+      
       {/* Add your components here */}
     </div>
   );
